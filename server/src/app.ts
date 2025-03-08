09863516343b2399b45ed87b250bf22013f8ec91
Snehal Saurabh <snehalsaurabh.code@gmail.com>
2025-03-08 21:47:10 +0530

    Upload & Download functionality complete.

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,10 +6,4 @@ const app = express();
 app.use(express.json());
 app.use('/api/files', fileRoutes);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 export default app;
